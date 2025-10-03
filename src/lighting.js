efda49bb714546e1b8c5d3eca89ab88c44903171
tomiconicgit <tomiconicx@icloud.com>
2025-10-03 19:26:45 +0100

    Create lighting.js

---
+++ src/lighting.js
@@ -0,0 +1,16 @@
+// file: src/lighting.js
+import * as THREE from 'three';
+
+export function initLighting(scene) {
+  const dirLight = new THREE.DirectionalLight(0xffffff, 2.0);
+  dirLight.castShadow = true;
+
+  const ambientLight = new THREE.AmbientLight(0x445566, 0.6);
+  scene.add(dirLight, ambientLight);
+
+  const lightTarget = new THREE.Object3D();
+  scene.add(lightTarget);
+  dirLight.target = lightTarget;
+  
+  return { dirLight, lightTarget };
+}
